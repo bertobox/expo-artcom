33e4b1f3e0712b72e4fd40dcc3c11e5656494cf1
Gabriel Trisca <tog000@gmail.com>
2011-07-04 00:36:36 -0400

    adding sample config file

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+	global $configuration;
+	$configuration['db']    = 'expo_artcom';       //database name
+	$configuration['host']  = 'localhost';  //database host
+	$configuration['user']  = 'root';       //database user
+	$configuration['pass']  = '';       //database user
+?>
